aebe289fed53ab6a216cb99eabc1d7c46f3aa740
runembp <58623004+runembp@users.noreply.github.com>
2021-03-12 13:25:56 +0100

    Initial commit

---
+++ src/main/java/com/adventurealley/aafcro/AafcroApplication.java
@@ -0,0 +1,15 @@
+package com.adventurealley.aafcro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AafcroApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AafcroApplication.class, args);
+    }
+
+}
