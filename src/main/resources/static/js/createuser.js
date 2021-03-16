fa5943377d4c0a919ad4fadec23d015c9a890200
runembp <runembpetersen@gmai.com>
2021-03-16 16:09:12 +0100

    Forbedring af Fetch

--- src/main/resources/static/js/createuser.js
+++ src/main/resources/static/js/createuser.js
@@ -3,19 +3,15 @@ const saveButton = document.getElementById("save");
 saveButton.addEventListener("click", createUser)
 
 function validatePassword() {
-
     if(document.getElementById("password1").value === document.getElementById("password2").value) {
-
         return true
     }
-
     return false
 }
 
-function emailExists() {
+async function emailExists() {
 
-    const getEmailToValidation = document.getElementById("email").value
-
+    const url = "http://localhost:8080/getAllUsers";
     const getObject = {
         headers: {
             "Content-type": 'application/json'
@@ -23,74 +19,79 @@ function emailExists() {
         method: "GET"
     }
 
-    return new Promise(resolve =>
-    fetch("http://localhost:8080/getAllUsers", getObject)
-        .then(email => {
-            return email.json()
-        }).then(data => {
-            let list = data.map(x => x.email)
-            resolve(list)
+    return await new Promise(resolve =>
+        fetch(url, getObject)
+        .then(email => email.json())
+        .then(data => {
+            resolve(data.map(x => x.email))
         }))
-
-
 }
 
-function validateEmail(data) {
-    let list = ""
-
-    emailExists().then(data => {
-        list = data;
-    })
-
-    console.log(list)
-}
-
-function createUser()
+async function testing()
 {
-    if(!validatePassword()) {
+    const url = "http://localhost:8080/getAllUsers";
+    const getObject = {
+        headers: {
+            "Content-type": 'application/json'
+        },
+        method: "GET"
+    }
+
+    const response = await fetch(url, getObject);
+    let users = await response.json();
+    users = users.map(x => x.email)
+
+    if(users.includes(document.getElementById("email")))
+    {
+        console.log("Email found!")
+        return true;
+    }
+    return false;
+
+}
+
+testing();
+
+function createUser() {
+    if (!validatePassword()) {
         console.log("You fucked up the password")
         return;
     }
 
-    if(emailExists()) {
+    if (emailExists()) {
         console.log("You fucked up the email")
         return;
 
-    const url = "http://localhost:8080/postUser";
+        const url = "http://localhost:8080/postUser";
 
-    const newUser = {
-        "email": document.getElementById("email").value,
-        "password":document.getElementById("password1").value,
-        "firstName":document.getElementById("firstName").value,
-        "lastName":document.getElementById("lastName").value,
-        "dateOfBirth":document.getElementById("birthdate").value,
-        "height":document.getElementById("height").value
+        const newUser = {
+            "email": document.getElementById("email").value,
+            "password": document.getElementById("password1").value,
+            "firstName": document.getElementById("firstName").value,
+            "lastName": document.getElementById("lastName").value,
+            "dateOfBirth": document.getElementById("birthdate").value,
+            "height": document.getElementById("height").value
+        }
+
+        const newUserStringified = JSON.stringify(newUser);
+
+        const postObject = {
+            headers:
+                {
+                    "Content-type": 'application/json'
+                },
+            method: 'POST',
+            body: newUserStringified
+        };
+
+        fetch(url, postObject)
+            .then(response => response.json())
+            .then(response => {
+                console.log("success")
+                //  window.location.href = "/"
+            })
+            .catch(response => {
+                console.log("failure")
+            })
     }
-
-    const newUserStringified = JSON.stringify(newUser);
-
-    const postObject = {
-        headers:
-            {
-                "Content-type": 'application/json'
-            },
-        method: 'POST',
-        body: newUserStringified
-    };
-
-    fetch(url, postObject)
-        .then(response => response.json())
-        .then(response => {
-            console.log("success")
-          //  window.location.href = "/"
-        })
-        .catch(response => {
-            console.log("failure")
-        })
 }
-
-
-
-
-
-
