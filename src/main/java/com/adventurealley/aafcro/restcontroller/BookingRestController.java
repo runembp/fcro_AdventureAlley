fdd448946e9339e36cea7e8919a17c43ec5996dd
runembp <58623004+runembp@users.noreply.github.com>
2021-03-17 23:10:20 +0100

    Implementation af findAllBookingsByActivity/{id} til RestController

--- src/main/java/com/adventurealley/aafcro/restcontroller/BookingRestController.java
+++ src/main/java/com/adventurealley/aafcro/restcontroller/BookingRestController.java
@@ -3,12 +3,10 @@ package com.adventurealley.aafcro.restcontroller;
 import com.adventurealley.aafcro.model.BookingModel;
 import com.adventurealley.aafcro.repository.IBookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class BookingRestController
@@ -20,6 +18,12 @@ public class BookingRestController
     public List<BookingModel> findAllBookings()
     {
         return bookingRepository.findAll();
+    }
+
+    @GetMapping("findAllBookingsByActivity/{id}")
+    public Set<BookingModel> findAllBookingsByActivity(@PathVariable Long id)
+    {
+        return bookingRepository.findAllBookingsForActivity(id);
     }
 
     @PostMapping(value = "/postBooking", consumes = "application/json")
