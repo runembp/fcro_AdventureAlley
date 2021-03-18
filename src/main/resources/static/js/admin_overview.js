1591f6d071885e4774562add19030e7648ee87c5
GenuinelyMeizner <frederikmpetersen@gmail.com>
2021-03-18 14:40:30 +0100

    Knapper er blevet tilføjet til og redigere, gemme og slette, mangler funktionalitet

---
+++ src/main/resources/static/js/admin_overview.js
@@ -0,0 +1,189 @@
+window.onload = async function createAdminOverview() {
+
+    let updateVar = localStorage.getItem("updateState");
+
+    const url = "http://localhost:8080/findAllActivities";
+
+    const activity_container = document.getElementById("activity_container")
+
+    const getObject = {
+        method:"GET",
+        headers: {
+            "Content-type":"application/json"
+        }
+    }
+
+    const response = await fetch(url, getObject);
+    const data = await response.json();
+
+    let activityList = data;
+
+    for(let i = 0; i < activityList.length; i++)
+    {
+        if(i % 2 === 0)
+        {
+            insertActivityToLeftColumn(activityList[i])
+        }
+        else
+        {
+            insertActivityToRightColumn(activityList[i])
+        }
+    }
+
+    function insertActivityToLeftColumn(activity)
+    {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        activity_container.appendChild(row);
+
+        const col_left = document.createElement('div');
+        col_left.setAttribute('class', 'col');
+        row.appendChild(col_left);
+
+        const h1 = document.createElement('h1');
+        h1.setAttribute('class', 'text-danger ms-5');
+        if (updateVar === "1"){
+            h1.setAttribute('contentEditable', "true");
+        }
+        h1.textContent = activity.title;
+        col_left.appendChild(h1);
+
+        const updateButton = document.createElement('button');
+        updateButton.setAttribute('class', 'btn btn-primary ms-5');
+        updateButton.setAttribute('id', 'updateButton');
+        updateButton.innerHTML = "Update";
+        col_left.appendChild(updateButton);
+
+        const saveUpdateButton = document.createElement("button");
+        saveUpdateButton.setAttribute('class', 'btn btn-success ms-5');
+        saveUpdateButton.setAttribute('id', 'saveUpdateButton');
+        saveUpdateButton.innerHTML = "Save";
+        col_left.appendChild(saveUpdateButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute('class', 'btn btn-warning ms-5');
+        deleteButton.setAttribute('id', 'deleteButton');
+        deleteButton.innerHTML = "Delete";
+        col_left.appendChild(deleteButton);
+
+        const p = document.createElement('p');
+        p.setAttribute('class', 'text-danger ms-5')
+        if (updateVar === "1") {
+            p.setAttribute('contentEditable', 'true');
+        }
+        p.textContent = activity.description;
+        col_left.appendChild(p);
+
+        const p_price = document.createElement('p');
+        p_price.setAttribute('class', 'text-danger ms-5 fw-bold');
+        if (updateVar === "1") {
+            p_price.setAttribute('contentEditable', 'true');
+        }
+        p_price.textContent = "Price: " + activity.price;
+        col_left.appendChild(p_price);
+
+        const p_height = document.createElement('p');
+        p_height.setAttribute('class', 'text-danger ms-5 fw-bold');
+        if (updateVar === "1") {
+            p_height.setAttribute('contentEditable', 'true');
+        }
+        p_height.textContent = "Minimum height: " + activity.minHeight;
+        col_left.appendChild(p_height);
+
+        const col_right = document.createElement('div');
+        col_right.setAttribute('class', 'col text-center');
+        row.appendChild(col_right);
+
+        const img = document.createElement('img');
+        img.setAttribute('class', 'h-100 w-50 p-4');
+        img.setAttribute('src', activity.imageLink);
+        col_right.appendChild(img);
+    }
+
+    function insertActivityToRightColumn(activity) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        activity_container.appendChild(row);
+
+        const col_left = document.createElement('div');
+        col_left.setAttribute('class', 'col text-center');
+        row.appendChild(col_left);
+
+        const col_right = document.createElement('div');
+        col_right.setAttribute('class', 'col');
+        row.appendChild(col_right);
+
+        const h1 = document.createElement('h1');
+        h1.setAttribute('class', 'text-danger' + ' ms-5');
+        if (updateVar === "1") {
+            h1.setAttribute('contentEditable', 'true');
+        }
+        h1.textContent = activity.title;
+        col_right.appendChild(h1);
+
+        const updateButton = document.createElement('button');
+        updateButton.setAttribute('class', 'btn btn-primary ms-5');
+        updateButton.setAttribute('id', 'updateButton');
+        updateButton.innerHTML = "Update";
+        col_right.appendChild(updateButton);
+
+        const saveUpdateButton = document.createElement("button");
+        saveUpdateButton.setAttribute('class', 'btn btn-success ms-5');
+        saveUpdateButton.setAttribute('id', 'saveUpdateButton');
+        saveUpdateButton.innerHTML = "Save";
+        col_right.appendChild(saveUpdateButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute('class', 'btn btn-warning ms-5');
+        deleteButton.setAttribute('id', 'deleteButton');
+        deleteButton.innerHTML = "Delete";
+        col_right.appendChild(deleteButton);
+
+        const p = document.createElement('p');
+        p.setAttribute('class', 'text-danger ms-5')
+        if (updateVar === "1") {
+            p.setAttribute('contentEditable', 'true');
+        }
+        p.textContent = activity.description;
+        col_right.appendChild(p);
+
+        const p_price = document.createElement('p');
+        p_price.setAttribute('class', 'text-danger ms-5 fw-bold');
+        if (updateVar === "1") {
+            p_price.setAttribute('contentEditable', 'true');
+        }
+        p_price.textContent = "Price: " + activity.price;
+        col_right.appendChild(p_price);
+
+        const p_height = document.createElement('p');
+        p_height.setAttribute('class', 'text-danger ms-5 fw-bold');
+        if (updateVar === "1") {
+            p_height.setAttribute('contentEditable', 'true');
+        }
+        p_height.textContent = "Minimum height: " + activity.minHeight;
+        col_right.appendChild(p_height);
+
+        const img = document.createElement('img');
+        img.setAttribute('class', 'h-100 w-50 p-4');
+        img.setAttribute('src', activity.imageLink);
+        col_left.appendChild(img);
+    }
+
+    document.querySelectorAll('#updateButton').forEach(updateButton => {
+        updateButton.addEventListener('click', changeUpdateStateToTrue);
+    });
+
+    function changeUpdateStateToTrue() {
+        localStorage.setItem("updateState", "1");
+        location.reload();
+    }
+
+    document.querySelectorAll('#saveUpdateButton').forEach(saveUpdateButton => {
+        saveUpdateButton.addEventListener('click', changeUpdateStateToFalse);
+    });
+
+    function changeUpdateStateToFalse() {
+        localStorage.setItem("updateState", "0");
+        location.reload();
+    }
+}
