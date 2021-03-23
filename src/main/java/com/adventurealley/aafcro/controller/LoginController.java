e91caa4d1ea65157f74544b9414b514cd81eb79c
runembp <58623004+runembp@users.noreply.github.com>
2021-03-23 11:57:10 +0100

    Implementation af bruger-data overblik på Booking siden
    Fjernet brug af UserRepository i UserRestController og sat det i USerService istedet

--- src/main/java/com/adventurealley/aafcro/controller/LoginController.java
+++ src/main/java/com/adventurealley/aafcro/controller/LoginController.java
@@ -1,18 +1,11 @@
 package com.adventurealley.aafcro.controller;
 
-import com.adventurealley.aafcro.model.UserModel;
 import com.adventurealley.aafcro.service.LoginService;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-
-import java.security.Principal;
-
 
 @Controller
 public class LoginController
@@ -25,7 +18,6 @@ public class LoginController
     {
         return "login";
     }
-
 
     @PostMapping("/postlogin")
     public String postLogin(WebRequest data)
@@ -45,6 +37,6 @@ public class LoginController
     @GetMapping("/loggedin/{email}")
     public String getLoggedIn()
     {
-        return "loggedin/index";
+        return "loggedin/bookings";
     }
 }
---
+++ src/main/java/com/adventurealley/aafcro/service/UserService.java
@@ -0,0 +1,30 @@
+package com.adventurealley.aafcro.service;
+
+import com.adventurealley.aafcro.model.UserModel;
+import com.adventurealley.aafcro.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService
+{
+    @Autowired
+    IUserRepository userRepository;
+
+    public UserModel getUserByEmail(String userEmail)
+    {
+        return userRepository.findUserModelByEmail(userEmail);
+    }
+
+    public List<UserModel> findAll()
+    {
+        return userRepository.findAll();
+    }
+
+    public UserModel save(UserModel userModel)
+    {
+        return userRepository.save(userModel);
+    }
+}
