041688e071db9cf76119f6150d13ab7c39e0bbdf
runembp <58623004+runembp@users.noreply.github.com>
2021-03-23 21:13:20 +0100

    Implementation af Kalender Oversigt over Bookings

--- src/main/java/com/adventurealley/aafcro/controller/BookingController.java
+++ src/main/java/com/adventurealley/aafcro/controller/BookingController.java
@@ -2,6 +2,7 @@ package com.adventurealley.aafcro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -13,9 +14,17 @@ public class BookingController
         return "loggedin/bookings";
     }
 
-    @GetMapping("/loggedin/booking_calendar")
-    public String getBookingCalendar(WebRequest data)
+    @GetMapping("/loggedin/booking_calendar/{email}")
+    public String getBookingCalendar()
     {
         return "loggedin/booking_calendar";
+    }
+
+    @PostMapping("loggedin/postCalendar")
+    public String postCalendar(WebRequest data)
+    {
+        String email = data.getParameter("postCalendar");
+
+        return "redirect:/loggedin/booking_calendar/"+email;
     }
 }
