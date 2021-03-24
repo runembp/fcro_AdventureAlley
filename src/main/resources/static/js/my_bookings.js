71fedb5a85a3d5f52fa07b0f47f470e438998e3a
christina bartholomæussen <chri782z@edu.ucl.dk>
2021-03-24 11:58:24 +0100

    Dynamisk tabel implementeret

--- src/main/resources/static/js/my_bookings.js
+++ src/main/resources/static/js/my_bookings.js
@@ -1,4 +1,4 @@
-const dropdownMyBookings = document.querySelector("#dropdownMyBooking");
+
 const useremail = document.getElementById("useremail");
 
 const email = window.location.pathname.substring(19);
@@ -7,33 +7,48 @@ const userUrl = `http://localhost:8080/bookings/${email}`;
 
 getBookingToUser();
 
-async function getBookingToUser(){
 
+async function getBookingToUser() {
+
+    const bookingOverviewTable = document.getElementById("bookingOverview");
     const response = await fetch(userUrl);
     const bookings = await response.json();
 
+    for (let i = 0; i < bookings.length; i++) {
 
-    for(let i = 0; i < bookings.length; i++)
-{
-    let bookingId = bookings[i].bookingId;
+        let row = bookingOverviewTable.insertRow();
 
-    //Bruges til at hente aktivitet til den pågælgende booking
-    const activityUrl = `http://localhost:8080/getActivityToBooking/${bookingId}`;
-    const response1 = await fetch(activityUrl)
-    const activity = await response1.json();
+        let bookingId = bookings[i].bookingId;
 
-    //Bruges til at hente timeslot for booking
-    const timeslotUrl = `http://localhost:8080/timeslotForBooking/${bookingId}`;
-    const repsonse2 = await fetch(timeslotUrl);
-    const timeslot = await  repsonse2.json();
+        //Bruges til at hente aktivitet til den pågælgende booking
+        const activityUrl = `http://localhost:8080/getActivityToBooking/${bookingId}`;
+        const response1 = await fetch(activityUrl)
+        const activity = await response1.json();
 
-    let element = document.createElement("p");
-    element.innerText = activity.title + ' ' + bookings[i].bookingDate + ' '+ timeslot.start + ' ' + timeslot.end;
+        //Bruges til at hente timeslot for booking
+        const timeslotUrl = `http://localhost:8080/timeslotForBooking/${bookingId}`;
+        const repsonse2 = await fetch(timeslotUrl);
+        const timeslot = await repsonse2.json();
 
-    dropdownMyBookings.appendChild(element);
-    };
+
+        const date = row.insertCell(0);
+        date.innerHTML = bookings[i].bookingDate;
+
+        const activityTitle = row.insertCell(1);
+        activityTitle.innerHTML = activity.title;
+
+        const activityStart = row.insertCell(2);
+        activityStart.innerHTML = timeslot.start;
+
+        const activityEnd = row.insertCell(3);
+        activityEnd.innerHTML = timeslot.end;
+
+        const cancel = row.insertCell(4);
+        let cancelButton = document.createElement("button");
+        cancelButton.innerHTML = "Aflys Booking";
+        cancel.appendChild(cancelButton);
+    }
 }
-
 
 
 
