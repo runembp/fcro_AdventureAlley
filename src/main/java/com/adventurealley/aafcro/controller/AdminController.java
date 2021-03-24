e0e4afab1a74a6205586c0ed2b540509888d60ac
runembp <58623004+runembp@users.noreply.github.com>
2021-03-24 14:09:03 +0100

    Rettelser per code review

--- src/main/java/com/adventurealley/aafcro/controller/AdminController.java
+++ src/main/java/com/adventurealley/aafcro/controller/AdminController.java
@@ -1,16 +1,14 @@
 package com.adventurealley.aafcro.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController {
-
+public class AdminController
+{
     @GetMapping("/adminCRUD")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String createActivity(){
+    public String createActivity()
+    {
         return "/admin/admin_crud_activity";
     }
-
 }
