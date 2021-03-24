f2a6dc86bb486ad9bafaaee136c4b4fb42c9d994
runembp <58623004+runembp@users.noreply.github.com>
2021-03-24 08:45:37 +0100

    Implementation af Kalender-knapper til Fragment og Bookings side, samt Mappings så de modtager emailen korrekt

--- src/main/java/com/adventurealley/aafcro/controller/BookingController.java
+++ src/main/java/com/adventurealley/aafcro/controller/BookingController.java
@@ -20,11 +20,27 @@ public class BookingController
         return "loggedin/booking_calendar";
     }
 
-    @PostMapping("loggedin/postCalendar")
+    @PostMapping("/loggedin/postCalendar")
     public String postCalendar(WebRequest data)
     {
         String email = data.getParameter("postCalendar");
 
-        return "redirect:/loggedin/booking_calendar/"+email;
+        return "redirect:/loggedin/booking_calendar/" + email;
+    }
+
+    @PostMapping("/loggedin/postBookings")
+    public String postBookings(WebRequest data)
+    {
+        String email = data.getParameter("postBookings");
+
+        return "redirect:/loggedin/bookings/" + email;
+    }
+
+    @PostMapping("/loggedin/postBookingsCalendar")
+    public String postBookingsCalendar(WebRequest data)
+    {
+        String email = data.getParameter("postBookings");
+
+        return "redirect:/loggedin/bookings/" + email;
     }
 }
