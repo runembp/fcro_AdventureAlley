533cbed5ab5ed28724675820fdc0e9542aafa27b
runembp <runembpetersen@gmai.com>
2021-03-26 07:42:07 +0100

    Backup af Branch

--- src/main/resources/static/js/admin/admin_booking_overview.js
+++ src/main/resources/static/js/admin/admin_booking_overview.js
@@ -1,19 +1,18 @@
-const bookingsContainer = document.getElementById("bookingContainer");
+const bookingTable = document.getElementById("bookingTable")
 
 async function getAllBookings()
 {
-    const url = "http://localhost:8080/findAllBookingsForUsers";
+    const url = "http://localhost:8080/findAllBookingsForAllUsers";
     return await fetch(url).then(x => x.json());
 }
 
-let bookingList = getAllBookings().then(x =>
+function populateBookingTable(bookingList)
 {
-    bookingList = x;
+    const row = bookingTable.insertRow;
 
-    for(let i = 0; i < bookingList.length;i++)
-    {
-        console.log(bookingList[i]);
-    }
+}
+const title = row.insertCell(0);
+title.innerHTML = "Booking title";
 
-});
-
+const date = row.insertCell(1);
+date.innerHTML = "Booking date";
